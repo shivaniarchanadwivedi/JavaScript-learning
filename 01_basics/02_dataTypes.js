ee52b1fb07b83870653ee7b8d8af6ebc82cf7b11
shivaniarchanadwivedi <60256426+shivaniarchanadwivedi@users.noreply.github.com>
2024-08-12 09:16:22 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treats all js code as a newer version
+
+//alert("hello"); //  we are using node js not browser
+
+let name = "Shivani";
+let age = 25;
+let isLoggedIn = false;
+
+//number =>2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
